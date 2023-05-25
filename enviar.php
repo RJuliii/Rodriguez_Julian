4ec0296a3633c42533292f7f2e5d759620e07e48
JulianRDZ <rodriguezjuliann@outlook.com>
2023-05-25 18:08:54 -0300

    contacto

--- enviar.php
+++ enviar.php
@@ -1,24 +1,11 @@
 <?php
-$name = $_POST['name'];
-$mail = $_POST['mail'];
-$phone = $_POST['phone'];
-$message = $_POST['message'];
-
-$header = 'From: ' . $mail . " \r\n";
-$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
-$header .= "Mime-Version: 1.0 \r\n";
-$header .= "Content-Type: text/plain";
-
-$message = "Este mensaje fue enviado por: " . $name . " \r\n";
-$message .= "Su e-mail es: " . $mail . " \r\n";
-$message .= "Teléfono de contacto: " . $phone . " \r\n";
-$message .= "Mensaje: " . $_POST['message'] . " \r\n";
-$message .= "Enviado el: " . date('d/m/Y', time());
-
-$para = 'rodriguezjuliann@outlook.com';
-$asunto = 'Asunto del mensaje';
-
-mail($para, $asunto, utf8_decode($message), $header);
-
-header("Location:index.html");
+    $destino= "rodriguezjuliann@outlook.com";
+    $nombre = $_POST["nombre"];
+    $apellido = $_POST["apellido"];
+    $correo = $_POST["correo"];
+    $telefono = $_POST["telefono"];
+    $mensaje = $_POST["mensaje"];
+    $contenido = "Nombre: " .$nombre . "\nApellido: " .$apellido . "\nCorreo: " .$correo . "\nTeléfono: " .$telefono . "\nMensaje: " . $mensaje;
+    mail($destino, "ContactoCV", $contenido);
+    header("Location:gracias.html");
 ?>
